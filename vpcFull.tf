e0860ff840f6051da2a4da7ba9741c02a57b371e
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2023-04-03 10:52:45 +0530

    Update vpcFull.tf

--- vpcFull.tf
+++ vpcFull.tf
@@ -2,43 +2,43 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc1" {
+  cidr_block = "${var.cidr}"
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = {
-    Name = "MyVPC"
+    Name = "${var.VPCtag}"
   }
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "IGWY1" {
+  vpc_id = aws_vpc.IGY1.id
   tags = {
-    Name = "MyIGW"
+    Name = "${var.igwytag"
   }
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "RT1" {
+  vpc_id = aws_vpc.vpc1.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.IGWY1.id
   }
   tags = {
-    Name = "MyRouteTable"
+    Name = "${var.RTTag}"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "sb1" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "${var.scidr}"
   availability_zone = "us-west-2a"
   tags = {
-    Name = "MySubnet"
+    Name = "${var.SubnetTag}"
   }
 }
 
 resource "aws_route_table_association" "my_route_table_association" {
-  subnet_id = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+  subnet_id = aws_subnet.sb1.id
+  route_table_id = aws_route_table.RT1.id
 }
