f85b0704b51a5cd02441f1a362fc097fe1bd6a67
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2023-05-16 11:07:06 +0530

    Create t2state.py

---
+++ t2state.py
@@ -0,0 +1,28 @@
+import json
+
+def extract_main_tf(terraform_tfstate_file):
+    with open(terraform_tfstate_file, 'r') as f:
+        tfstate_data = json.load(f)
+
+    resources = tfstate_data['resources']
+
+    main_tf_content = ""
+
+    for resource in resources:
+        if resource['type'] == 'terraform_remote_state':
+            # Skip remote state resources
+            continue
+        
+        # Retrieve the resource configuration
+        config = resource['instances'][0]['attributes']['config']
+        
+        # Check if 'main.tf' exists in the resource configuration
+        if 'main.tf' in config:
+            main_tf_content += config['main.tf']
+
+    return main_tf_content
+
+# Example usage
+terraform_tfstate_file = 'path/to/terraform.tfstate'
+main_tf_content = extract_main_tf(terraform_tfstate_file)
+print(main_tf_content)
